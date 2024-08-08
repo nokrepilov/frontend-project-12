ef1ff7868e491fa9e0f460da127048947cc137be
Nikita Okrepilov <nokrepilov@yandex.ru>
2024-08-08 15:05:00 +0300

    fourth mentor chek and big upgrade

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -11,7 +11,7 @@ import HeaderListMessages from './HeaderListMessages.jsx';
 import ListMessages from './ListMessages.jsx';
 import NewMessageForm from '../Forms/NewMessageForm.jsx';
 
-const Messages = () => {
+export const Messages = () => {
   const dispatch = useDispatch();
 
   const { t } = useTranslation();
@@ -80,5 +80,3 @@ const Messages = () => {
     </>
   );
 };
-
-export default Messages;
