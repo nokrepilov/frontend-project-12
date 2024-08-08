22736c1da071217dcf0c46831786a6871795586a
Nikita Okrepilov <nokrepilov@yandex.ru>
2024-08-08 15:17:59 +0300

    fourth mentor chek and big upgrade

--- frontend/src/components/channels/Channels.jsx
+++ frontend/src/components/channels/Channels.jsx
@@ -26,7 +26,7 @@ import ChannelContext from '../../contexts/ChannelContext.js';
 
 import ChannelModalContext from '../../contexts/ChannelModalContext.js';
 
-export const Channels = () => {
+export function Channels() {
   const { t } = useTranslation();
 
   const dispatch = useDispatch();
@@ -101,4 +101,4 @@ export const Channels = () => {
       </ChannelModalContext.Provider>
     </>
   );
-};
+}
